b35cd8999b2d060312269b6de7d10d183d22e387
merail <r.mesherov@blanc.ru>
2024-01-13 16:09:21 +0300

    Animate color changing

--- app/src/main/java/me/rail/drawing_switch/MainActivity.kt
+++ app/src/main/java/me/rail/drawing_switch/MainActivity.kt
@@ -3,18 +3,22 @@ package me.rail.drawing_switch
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import me.rail.drawing_switch.ui.theme.DrawingSwitchTheme
@@ -45,16 +49,35 @@ fun Main() {
 fun BackgroundShape() {
     val disabledSwitchBackgroundColor = DrawingSwitchTheme.colors.disabledSwitchBackgroundColor
     val switchColor = DrawingSwitchTheme.colors.switchColor
+    val enabledSwitchBackgroundColor = DrawingSwitchTheme.colors.enabledSwitchBackgroundColor
+
+    val isEnabled = remember {
+        mutableStateOf(
+            value = false,
+        )
+    }
+
+    val backgroundColor by animateColorAsState(
+        targetValue = if (isEnabled.value) enabledSwitchBackgroundColor else disabledSwitchBackgroundColor,
+        animationSpec = tween(
+            durationMillis = 1000,
+        ),
+        label = "BackgroundColorAnimation",
+    )
+
     Canvas(
         modifier = Modifier
             .size(
                 width = 234.dp,
                 height = 100.dp,
-            ),
+            )
+            .clickable {
+                isEnabled.value = !isEnabled.value
+            },
     ) {
         drawRoundRect(
             brush = SolidColor(
-                value = disabledSwitchBackgroundColor,
+                value = backgroundColor,
             ),
             cornerRadius = CornerRadius(
                 x = 60.dp.toPx(),
